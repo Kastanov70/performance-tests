e2436099e9fd5099d2fe7d9a56edef61d7834b06
Kastanov70 <stepan.kastanov@gmail.com>
2025-11-26 15:16:42 +0300

    Введение в HTTPX. Практическое задание: работа с HTTPX

---
+++ httpx_open_deposit_account.py
@@ -0,0 +1,33 @@
+# httpx_open_deposit_account.py
+import time
+
+import httpx
+
+
+with httpx.Client() as client:
+    # Создайте нового пользователя.
+    create_user_payload = {
+        "email": f"user.{time.time()}@example.com",
+        "lastName": "string",
+        "firstName": "string",
+        "middleName": "string",
+        "phoneNumber": "string",
+    }
+    create_user_response = httpx.post(
+        "http://localhost:8003/api/v1/users", json=create_user_payload
+    )
+    create_user_response_data = create_user_response.json()
+
+    # Откройте депозитный счёт.
+    create_deposit_account_payload = {
+        "userId": f"{create_user_response_data['user']['id']}"
+    }
+    create_deposit_account_response = httpx.post(
+        "http://localhost:8003/api/v1/accounts/open-deposit-account",
+        json=create_deposit_account_payload,
+    )
+    create_deposit_account_data = create_deposit_account_response.json()
+
+    # Выведите результат в консоль.
+    print("Create Account response:", create_deposit_account_data)
+    print("Status Code:", create_deposit_account_response.status_code)
