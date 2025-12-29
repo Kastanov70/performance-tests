b01a106f5459f2282a4035928b30e5c8762bd178
Kastanov70 <stepan.kastanov@gmail.com>
2025-12-29 10:48:24 +0300

    Знакомство с Pydantic. Практика написания Pydantic-моделей

--- pydantic_create_user.py
+++ pydantic_create_user.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 
 
 class UserSchema(BaseModel):
@@ -6,21 +6,22 @@ class UserSchema(BaseModel):
     Схема для представления данных пользователя.
 
     Используется для отображения информации о пользователе при чтении данных.
+    Поддерживает работу с данными в формате camelCase через алиасы полей.
 
     Attributes:
         id (str): Уникальный идентификатор пользователя в системе.
         email (EmailStr): Адрес электронной почты пользователя (валидируется на корректность формата).
-        lastName (str): Фамилия пользователя.
-        firstName (str): Имя пользователя.
-        middleName (str): Отчество пользователя.
-        phoneNumber (str): Номер телефона пользователя в строковом формате.
+        last_name (str): Фамилия пользователя (алиас 'lastName' для camelCase).
+        first_name (str): Имя пользователя (алиас 'firstName' для camelCase).
+        middle_name (str): Отчество пользователя (алиас 'middleName' для camelCase).
+        phone_number (str): Номер телефона пользователя в строковом формате (алиас 'phoneNumber' для camelCase).
     """
     id: str
     email: EmailStr
-    lastName: str
-    firstName: str
-    middleName: str
-    phoneNumber: str
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+    phone_number: str = Field(alias="phoneNumber")
 
 
 class CreateUserRequestSchema(BaseModel):
@@ -29,19 +30,20 @@ class CreateUserRequestSchema(BaseModel):
 
     Используется как модель запроса для endpoint'а POST /api/v1/users создания пользователя.
     Все поля обязательны для заполнения.
+    Поддерживает прием данных в формате camelCase через алиасы полей.
 
     Attributes:
         email (EmailStr): Адрес электронной почты (валидируется на корректность формата).
-        lastName (str): Фамилия пользователя.
-        firstName (str): Имя пользователя.
-        middleName (str): Отчество пользователя.
-        phoneNumber (str): Номер телефона пользователя.
+        last_name (str): Фамилия пользователя (алиас 'lastName' для camelCase).
+        first_name (str): Имя пользователя (алиас 'firstName' для camelCase).
+        middle_name (str): Отчество пользователя (алиас 'middleName' для camelCase).
+        phone_number (str): Номер телефона пользователя (алиас 'phoneNumber' для camelCase).
     """
     email: EmailStr
-    lastName: str
-    firstName: str
-    middleName: str
-    phoneNumber: str
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+    phone_number: str = Field(alias="phoneNumber")
 
 
 class CreateUserResponseSchema(BaseModel):
@@ -49,6 +51,7 @@ class CreateUserResponseSchema(BaseModel):
     Схема для формирования ответа после успешного создания пользователя.
 
     Содержит объект созданного пользователя в формате UserSchema.
+    Используется в ответах API на запрос создания пользователя.
 
     Attributes:
         user (UserSchema): Объект с данными созданного пользователя.
