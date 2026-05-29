079e6df72d6cc5ba46b97da7129cf1387876f7dd
Kastanov70 <76150267+Kastanov70@users.noreply.github.com>
2026-05-29 14:47:45 +0300

    Настройки нагрузочных тестов с Pydantic

--- tools/locust/user.py
+++ tools/locust/user.py
@@ -1,4 +1,5 @@
 from locust import User, between
+from config import settings
 
 
 class LocustBaseUser(User):
@@ -8,4 +9,7 @@ class LocustBaseUser(User):
     """
     abstract = True
     host = "localhost"
-    wait_time = between(1,3)
+    wait_time = between(
+        min_wait=settings.locust_user.wait_time_min,
+        max_wait=settings.locust_user.wait_time_max
+    )
